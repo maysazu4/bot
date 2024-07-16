88ecbc5eaba0f005c25834d5ff70222675612c3f
maysazu4 <77391084+maysazu4@users.noreply.github.com>
2024-07-16 12:06:53 +0300

    connect to mogo atlas

--- basic_bot.py
+++ basic_bot.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import requests
 load_dotenv()
 SERVER_ADDRESS = os.getenv('SERVER_ADDRESS')
-
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Hello! This is your bot.')
@@ -20,14 +20,23 @@ async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         await update.message.reply_text("Failed to fetch data from server.")
 
+async def fetch_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    response = requests.get(f"{SERVER_ADDRESS}/generate")
+    if response.status_code == 200:
+        data = response.json()
+        await update.message.reply_text(f"Number generated and saved: {data['number']}")
+    else:
+        await update.message.reply_text("Failed to fetch data from server.")
+
 def main():
     # Create the Application and pass it your bot's token
-    application = Application.builder().token(bot_token.BOT_TOKEN).build()
+    application = Application.builder().token(BOT_TOKEN).build()
 
     # Register the /start command with the start function
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
-
+    fetch_data_handler = CommandHandler('fetchdata', fetch_data)
+    application.add_handler(fetch_data_handler)
     test_handler = CommandHandler('test', test)
     application.add_handler(test_handler)
 
